f4cc4edad77aa13b9d1a247ca4784be7a4928937
Oladayo Richard Ayomide <Oladayorichard1@gmail.com>
2025-05-08 22:01:29 +0100

    added new code

--- src/main/java/com/richard/renderapp/RenderappApplication.java
+++ src/main/java/com/richard/renderapp/RenderappApplication.java
@@ -2,8 +2,6 @@ package com.richard.renderapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class RenderappApplication {
@@ -11,14 +9,5 @@ public class RenderappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RenderappApplication.class, args);
 	}
-}
 
-// Add this REST controller class inside the same file or separately
-@RestController
-class HelloWorldController {
-
-	@GetMapping("/")
-	public String helloWorld() {
-		return "Hello, World";
-	}
 }
